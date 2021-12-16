12a00f80b45b0a775f9a42fd43abac2b8413c99e
Yeeun Ju <yeju@c10r6s11.42seoul.kr>
2021-12-16 17:43:17 +0900

    first commit

---
+++ get_next_line_bonus.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yeju <yeju@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/18 06:04:34 by yeju              #+#    #+#             */
+/*   Updated: 2021/04/21 19:18:20 by yeju             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+int					ft_strchr(char *string)
+{
+	int				i;
+
+	i = 0;
+	while (string[i])
+	{
+		if (string[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+int					gnl_split(char **string, char **line, int i)
+{
+	char			*tmp;
+	int				len;
+
+	(*string)[i] = '\0';
+	*line = ft_strdup(*string);
+	len = ft_strlen(*string + i + 1);
+	if (len == 0)
+	{
+		free(*string);
+		*string = 0;
+		return (1);
+	}
+	tmp = ft_strdup(*string + i + 1);
+	free(*string);
+	*string = tmp;
+	return (1);
+}
+
+int					return_this(char **string, char **line, int read_size)
+{
+	int				i;
+
+	if (read_size < 0)
+		return (-1);
+	if (*string && (i = ft_strchr(*string)) >= 0)
+		return (gnl_split(string, line, i));
+	else if (*string)
+	{
+		*line = *string;
+		*string = 0;
+		return (0);
+	}
+	*line = ft_strdup("");
+	return (0);
+}
+
+int					get_next_line(int fd, char **line)
+{
+	static char		*string[OPEN_MAX];
+	char			buffer[BUFFER_SIZE + 1];
+	int				read_size;
+	int				i;
+
+	if ((fd < 0) || (OPEN_MAX < fd) || (!line) || (BUFFER_SIZE <= 0))
+		return (-1);
+	while ((read_size = read(fd, buffer, BUFFER_SIZE)) > 0)
+	{
+		if (BUFFER_SIZE < read_size)
+			return (-1);
+		buffer[read_size] = '\0';
+		string[fd] = ft_strjoin(string[fd], buffer);
+		if ((i = ft_strchr(string[fd])) >= 0)
+			return (gnl_split(&string[fd], line, i));
+	}
+	return (return_this(&string[fd], line, read_size));
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yeju <yeju@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/07 14:40:29 by yeju              #+#    #+#             */
+/*   Updated: 2021/04/21 19:05:58 by yeju             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+# ifndef OPEN_MAX
+#  define OPEN_MAX 32
+# endif
+
+int				get_next_line(int fd, char **line);
+
+size_t			ft_strlen(const char *s);
+size_t			ft_strlcpy(char *dest, const char *src, size_t dstsize);
+size_t			ft_strlcat(char *dest, const char *src, size_t dstsize);
+char			*ft_strdup(char *s1);
+char			*ft_strjoin(char *s1, char *s2);
+
+#endif
